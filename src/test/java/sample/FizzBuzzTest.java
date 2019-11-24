f3af23dc44290e1abc44789c9826853854866333
makopi23 <kipersonalpc@gmail.com>
2019-11-24 22:17:14 +0900

    FizzBuzz Test

---
+++ src/test/java/sample/FizzBuzzTest.java
@@ -0,0 +1,51 @@
+package sample;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class FizzBuzzTest extends FizzBuzz {
+
+	@BeforeEach
+	void beforeEach() {
+		System.out.println("@BeforeEach()のテスト");
+	}
+
+	@AfterEach
+	void afterEach() {
+		System.out.println("@AfterEach()のテスト");
+	}
+
+	@BeforeAll
+	static void beforeAll() {
+		System.out.println("@BeforeAll()のテスト");
+	}
+
+	@AfterAll
+	static void afterAll() {
+		System.out.println("@@AfterAll()のテスト");
+	}
+
+	@Test
+	void 数値が3でも5でも割り切れる() {
+		assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(15));
+		assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(30));
+	}
+
+	@Test
+	void 数値が3で割り切れる() {
+		assertEquals("Fizz", FizzBuzz.fizzbuzz(3));
+		assertEquals("Fizz", FizzBuzz.fizzbuzz(6));
+	}
+
+	@Test
+	void 数値が5で割り切れる() {
+		assertEquals("Buzz", FizzBuzz.fizzbuzz(5));
+		assertEquals("Buzz", FizzBuzz.fizzbuzz(10));
+	}
+
+}
