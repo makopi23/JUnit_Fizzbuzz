ada337a45a597c510ae48892dc7899bc020128ca
makopi23 <kipersonalpc@gmail.com>
2019-11-25 12:08:05 +0900

    add maven-surefire-plugin to pom.xml

--- src/main/java/sample/FizzBuzz.java
+++ src/main/java/sample/FizzBuzz.java
@@ -2,24 +2,24 @@ package sample;
 
 public class FizzBuzz {
 
-	public static void main(String[] args) {
-		for(int i = 0; i < 100; i++) {
-			System.out.println(fizzbuzz(i));
-		}
+    public static void main(String[] args) {
+        for(int i = 0; i < 100; i++) {
+            System.out.println(fizzbuzz(i));
+        }
 
-	}
+    }
 
-	static String fizzbuzz(int number) {
+    static String fizzbuzz(int number) {
 
-		if ((number % 3 == 0) && (number % 5 == 0)) {
-			return "FizzBuzz";
-		}else if(number % 3 == 0) {
-			return "Fizz";
-		}else if(number % 5 == 0) {
-			return "Buzz";
-		}else {
-			return String.valueOf(number);
-		}
-	}
+        if ((number % 3 == 0) && (number % 5 == 0)) {
+            return "FizzBuzz";
+        }else if(number % 3 == 0) {
+            return "Fizz";
+        }else if(number % 5 == 0) {
+            return "Buzz";
+        }else {
+            return String.valueOf(number);
+        }
+    }
 
 }
--- src/test/java/sample/FizzBuzzTest.java
+++ src/test/java/sample/FizzBuzzTest.java
@@ -10,42 +10,42 @@ import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest extends FizzBuzz {
 
-	@BeforeEach
-	void beforeEach() {
-		System.out.println("@BeforeEach()のテスト");
-	}
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("@BeforeEach()のテスト");
+    }
 
-	@AfterEach
-	void afterEach() {
-		System.out.println("@AfterEach()のテスト");
-	}
+    @AfterEach
+    void afterEach() {
+        System.out.println("@AfterEach()のテスト");
+    }
 
-	@BeforeAll
-	static void beforeAll() {
-		System.out.println("@BeforeAll()のテスト");
-	}
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("@BeforeAll()のテスト");
+    }
 
-	@AfterAll
-	static void afterAll() {
-		System.out.println("@@AfterAll()のテスト");
-	}
+    @AfterAll
+    static void afterAll() {
+        System.out.println("@@AfterAll()のテスト");
+    }
 
-	@Test
-	void 数値が3でも5でも割り切れる() {
-		assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(15));
-		assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(30));
-	}
+    @Test
+    void 数値が3でも5でも割り切れる() {
+        assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(15));
+        assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(30));
+    }
 
-	@Test
-	void 数値が3で割り切れる() {
-		assertEquals("Fizz", FizzBuzz.fizzbuzz(3));
-		assertEquals("Fizz", FizzBuzz.fizzbuzz(6));
-	}
+    @Test
+    void 数値が3で割り切れる() {
+        assertEquals("Fizz", FizzBuzz.fizzbuzz(3));
+        assertEquals("Fizz", FizzBuzz.fizzbuzz(6));
+    }
 
-	@Test
-	void 数値が5で割り切れる() {
-		assertEquals("Buzz", FizzBuzz.fizzbuzz(5));
-		assertEquals("Buzz", FizzBuzz.fizzbuzz(10));
-	}
+    @Test
+    void 数値が5で割り切れる() {
+        assertEquals("Buzz", FizzBuzz.fizzbuzz(5));
+        assertEquals("Buzz", FizzBuzz.fizzbuzz(10));
+    }
 
 }
